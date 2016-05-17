ac8da6aa0e7911c43d6bfa920993da4b01eaf4eb
Daniel Rasmussen <danielrasmussen@Daniels-MacBook-Air.local>
2016-05-17 09:54:35 -0700

    have to use &&. otherwise loop may run indefinitely or past player/computer having 5 wins

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -59,7 +59,7 @@ function playToFive() {
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    while ((playerWins < 5) || (computerWins < 5)) {
+    while ((playerWins < 5) && (computerWins < 5)) {
 
     }
     return [playerWins, computerWins];
